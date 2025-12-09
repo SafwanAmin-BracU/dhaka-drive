d426840b63f9fcd52e076968274faecdb4728c93
Safwan Amin <safwan.amin@g.bracu.ac.bd>
2025-12-10 04:08:53 +0600

    refactor: configure PrismaClient with datasource URL from environment variables

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from '@prisma/client/edge';
+import { env } from '$env/dynamic/private';
 
-export const prisma = new PrismaClient();
+
+export const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url:
+                env.DATABASE_URL
+        }
+    }
+});
