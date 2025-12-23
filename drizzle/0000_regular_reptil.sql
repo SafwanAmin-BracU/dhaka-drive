1b08a75a0398daa612e15a8600b435ac956f9075
Safwan Amin <safwan.amin@g.bracu.ac.bd>
2025-12-24 04:36:06 +0600

    fix(database): ensure proper schema and extension setup in migration file

--- drizzle/0000_regular_reptil.sql
+++ drizzle/0000_regular_reptil.sql
@@ -1,3 +1,5 @@
 -- Custom SQL migration file, put your code below! --
+DROP SCHEMA IF EXISTS public CASCADE; CREATE SCHEMA public;
+DROP EXTENSION IF EXISTS postgis CASCADE;
 CREATE EXTENSION postgis;
 
