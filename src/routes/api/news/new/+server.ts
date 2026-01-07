7fed3e8747d2a8f803b3057a0e3c8076d28d6162
Tasin Imtiaj <tasin.imtiaj139@gmail.com>
2026-01-07 12:58:16 +0600

    feat: refactor POST handler to use FormData for news article creation

--- src/routes/api/news/new/+server.ts
+++ src/routes/api/news/new/+server.ts
@@ -1,16 +1,20 @@
-export const POST = async ({ locals: { drizzle, schema: { trafficNews } }, request }) => {
+import type { RequestHandler } from './$types';
+
+export const POST:RequestHandler = async ({ locals: { drizzle, schema: { trafficNews } }, request }) => {
     try {
-        const data = await request.json();
-        const title = data.title as string;
-        const content = data.content as string;
+
+        const formData = await request.formData();
+        const title = formData.get('title') as string;
+        const content = formData.get('content') as string;
+        const source = formData.get('source') as string;
+        console.log("Received data:", formData);
         if (!title || !content) {
             return new Response(JSON.stringify({ message: "Title and content are required." }), { status: 400 });
         }
         await drizzle.insert(trafficNews).values({
             title,
             content,
-            source: data.source || null,
-
+            source: source || null,
         });
         return new Response(JSON.stringify({ message: "News article created successfully." }), { status: 201 });
     } catch (err) {
